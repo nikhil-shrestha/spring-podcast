1169377bce5b19ed16d80ab67e58ec7a0307eed7
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-03-18 08:42:43 +0545

    removed podcast parser

--- src/main/java/com/azminds/podcast/Application.java
+++ src/main/java/com/azminds/podcast/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootPodcastApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootPodcastApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
