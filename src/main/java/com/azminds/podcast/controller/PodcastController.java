6990d398aec4ff41800a83fca70fe807fc936981
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-03-18 17:33:24 +0545

    added getter for podcast

--- src/main/java/com/azminds/podcast/controller/PodcastController.java
+++ src/main/java/com/azminds/podcast/controller/PodcastController.java
@@ -3,26 +3,34 @@ package com.azminds.podcast.controller;
 import com.azminds.podcast.exception.ResourceNotFoundException;
 import com.azminds.podcast.model.Podcast;
 import com.azminds.podcast.repository.PodcastRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/v1/")
 public class PodcastController {
+
+  private static final Logger logger = LoggerFactory.getLogger(PodcastController.class);
 
   @Autowired
   private PodcastRepository podcastRepository;
 
   //  get podcast
   @GetMapping("podcast")
-  public List<Podcast> listPodcast() {
-    return this.podcastRepository.findAll();
+  public Page<Podcast> listPodcast() {
+    Pageable pageable = PageRequest.of(0, 25, Sort.by("collectionId"));
+
+    return this.podcastRepository.findAll(pageable);
   }
 
   //  get podcast by ID
--- src/main/java/com/azminds/podcast/model/Podcast.java
+++ src/main/java/com/azminds/podcast/model/Podcast.java
@@ -170,88 +170,192 @@ public class Podcast implements Serializable {
         this.longDescription = longDescription;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getWrapperType() {
+        return wrapperType;
+    }
+
     public void setWrapperType(String wrapperType) {
         this.wrapperType = wrapperType;
+    }
+
+    public String getKind() {
+        return kind;
     }
 
     public void setKind(String kind) {
         this.kind = kind;
     }
 
+    public Long getCollectionId() {
+        return collectionId;
+    }
+
     public void setCollectionId(Long collectionId) {
         this.collectionId = collectionId;
+    }
+
+    public String getArtistName() {
+        return artistName;
     }
 
     public void setArtistName(String artistName) {
         this.artistName = artistName;
     }
 
+    public String getCollectionName() {
+        return collectionName;
+    }
+
     public void setCollectionName(String collectionName) {
         this.collectionName = collectionName;
+    }
+
+    public String getArtistViewUrl() {
+        return artistViewUrl;
     }
 
     public void setArtistViewUrl(String artistViewUrl) {
         this.artistViewUrl = artistViewUrl;
     }
 
+    public String getCollectionViewUrl() {
+        return collectionViewUrl;
+    }
+
     public void setCollectionViewUrl(String collectionViewUrl) {
         this.collectionViewUrl = collectionViewUrl;
+    }
+
+    public String getFeedUrl() {
+        return feedUrl;
     }
 
     public void setFeedUrl(String feedUrl) {
         this.feedUrl = feedUrl;
     }
 
+    public String getPreviewUrl() {
+        return previewUrl;
+    }
+
     public void setPreviewUrl(String previewUrl) {
         this.previewUrl = previewUrl;
+    }
+
+    public String getArtworkUrl30() {
+        return artworkUrl30;
     }
 
     public void setArtworkUrl30(String artworkUrl30) {
         this.artworkUrl30 = artworkUrl30;
     }
 
+    public String getArtworkUrl60() {
+        return artworkUrl60;
+    }
+
     public void setArtworkUrl60(String artworkUrl60) {
         this.artworkUrl60 = artworkUrl60;
+    }
+
+    public String getArtworkUrl100() {
+        return artworkUrl100;
     }
 
     public void setArtworkUrl100(String artworkUrl100) {
         this.artworkUrl100 = artworkUrl100;
     }
 
+    public String getArtworkUrl512() {
+        return artworkUrl512;
+    }
+
     public void setArtworkUrl512(String artworkUrl512) {
         this.artworkUrl512 = artworkUrl512;
+    }
+
+    public String getArtworkUrl600() {
+        return artworkUrl600;
     }
 
     public void setArtworkUrl600(String artworkUrl600) {
         this.artworkUrl600 = artworkUrl600;
     }
 
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
     public void setReleaseDate(String releaseDate) {
         this.releaseDate = releaseDate;
+    }
+
+    public Integer getTrackCount() {
+        return trackCount;
     }
 
     public void setTrackCount(Integer trackCount) {
         this.trackCount = trackCount;
     }
 
+    public String getCopyright() {
+        return copyright;
+    }
+
     public void setCopyright(String copyright) {
         this.copyright = copyright;
+    }
+
+    public String getCountry() {
+        return country;
     }
 
     public void setCountry(String country) {
         this.country = country;
     }
 
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
     public void setShortDescription(String shortDescription) {
         this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
     }
 
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getCurrentVersionReleaseDate() {
+        return currentVersionReleaseDate;
+    }
+
+    public void setCurrentVersionReleaseDate(String currentVersionReleaseDate) {
+        this.currentVersionReleaseDate = currentVersionReleaseDate;
+    }
+
+    public Integer getEpisodeCount() {
+        return episodeCount;
     }
 
     public void setEpisodeCount(Integer episodeCount) {
